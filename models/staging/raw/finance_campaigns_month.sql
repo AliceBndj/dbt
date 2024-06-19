3a0dd5a6396417626fb2fb2e1194df67ed3ec680
Alice Benadjemia <alice.bndj@gmail.com>
2024-06-19 13:38:25 +0000

    258

--- models/staging/raw/finance_campaigns_month.sql
+++ models/staging/raw/finance_campaigns_month.sql
@@ -4,8 +4,6 @@ ROUND(SUM(operational_margin-ads_cost),1) AS ads_margin,
 ROUND(SUM(average_basket),1) AS average_basket,
 ROUND(SUM(operational_margin),1) AS operational_margin,
 ROUND(SUM(ads_cost),1) AS ads_cost,
-FROM {{ref("finance_days")}}
-JOIN {{ref("int_campaigns_day")}}
-USING (date_date)
+FROM {{ref("finance_campaigns_day")}}
 GROUP BY month
 ORDER BY month DESC
